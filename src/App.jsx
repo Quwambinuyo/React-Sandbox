2b4e6440e914a69f21308d12b22be21fdb908ba0
Quwam Binuyo <qbinuyo7@gmail.com>
2025-01-27 14:16:32 +0100

    memory leak error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import UseRefExample1 from "./component/UseRefExample1";
 import UseRefExample2 from "./component/useRefExample2";
+import UseRefExample3 from "./component/useRefExample3";
 
 const App = () => {
   return (
     <div className="mt-5 ml-5">
       <UseRefExample1 />
-      <UseRefExample2 />{" "}
+      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 };
---
+++ src/component/useRefExample3.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Todo from "./Todo";
+import { useState } from "react";
+
+function useRefExample3() {
+  const [showTodo, setShowTod] = useState(true);
+
+  return (
+    <div>
+      {showTodo && <Todo />}
+      <button className="btn btn-primary" onClick={() => setShowTod(!showTodo)}>
+        Toggle Todo
+      </button>
+    </div>
+  );
+}
+
+export default useRefExample3;
